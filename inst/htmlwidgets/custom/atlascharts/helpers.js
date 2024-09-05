41ba50d9fb7f801cf8d96bc84d89e00ee3888ee2
Kamil Foltynski <kamil.foltynski@odysseusinc.com>
2024-09-05 15:31:38 +0000

    added helpers.js

---
+++ inst/htmlwidgets/custom/atlascharts/helpers.js
@@ -0,0 +1,47 @@
+function tooltipBuilder(d, data, colors_fun) {
+  const nameBuilder = (name, color) => `<span class="tip-name" style="background-color:${color}; color: ${name == 'end' ? 'black' : 'white'}">${name}</span>`;
+  const stepBuilder = (step) => `<div class="tip-step">${step.names.map(n => nameBuilder(n.name, n.color)).join("")}</div>`;
+
+  //const path = this.getPathToNode(d);
+  const path = getPathToNode2(d, data, colors_fun);
+  //debugger;
+
+  return `<div class="tip-container">${path.map(s => stepBuilder(s)).join("")}</div>`;
+}
+
+/*
+function getPathToNode(node) {
+	const eventCohorts = this.pathwaysObserver().eventCohorts;
+	const colors = this.pathwaysObserver().colors; // charts.colors()
+	let ancestors = this.getAncestors(node);
+	let pathway = ancestors.map(p => (p.data.name == "end") ? {names: [{name: "end", color: colors("end")}], count: p.value} : {
+		names: eventCohorts.filter(c => (c.code & Number.parseInt(p.data.name)) > 0)
+						.map(ec => ({name: ec.name, color: colors(ec.code)})), count: p.value});
+	return pathway;
+}
+*/
+
+function getAncestors(node) {
+	var path = [];
+	var current = node;
+	while (current.parent) {
+		path.unshift(current);
+		current = current.parent;
+	}
+	return path;
+}
+
+
+
+function getPathToNode2(node, data, colors_fun) {
+	//const eventCohorts = this.pathwaysObserver().eventCohorts;
+	const eventCohorts = data.eventCohorts;
+	//const colors = this.pathwaysObserver().colors; // charts.colors()
+	const colors = colors_fun; // charts.colors()
+	let ancestors = getAncestors(node);
+	let pathway = ancestors.map(p => (p.data.name == "end") ? {names: [{name: "end", color: colors("end")}], count: p.value} : {
+		names: eventCohorts.filter(c => (c.code & Number.parseInt(p.data.name)) > 0)
+						.map(ec => ({name: ec.name, color: colors(ec.code)})), count: p.value});
+	return pathway;
+}
+
